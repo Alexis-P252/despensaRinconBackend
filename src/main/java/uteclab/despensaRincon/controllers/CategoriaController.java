9becdfb4d50563438882ef5f06b59f1a8aa6106e
alexis <aleperalta252@gmail.com>
2023-04-26 11:42:06 -0300

    otros metodos en categoria y productos controller

--- src/main/java/uteclab/despensaRincon/controllers/CategoriaController.java
+++ src/main/java/uteclab/despensaRincon/controllers/CategoriaController.java
@@ -1,13 +1,17 @@
 package uteclab.despensaRincon.controllers;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 import uteclab.despensaRincon.entities.Categoria;
 import uteclab.despensaRincon.models.services.CategoriaService;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -50,9 +54,74 @@ public class CategoriaController {
             response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
             return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        response.put ("msg", "Datos del Categoria ingresados correctamente");
-        response.put("Categoria", newCategoria);
+        response.put ("msg", "Categoria creada correctamente");
+        response.put("categoria", newCategoria);
         return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@Valid @RequestBody Categoria categoria, BindingResult result, @PathVariable(value="id")Long id  ) {
+
+        Categoria categoriaActual = categoriaService.findById(id);
+
+        Map<String,Object> response = new HashMap<>();
+
+        if(categoriaActual == null) {
+            response.put("msg","No existe una categoria con id = ".concat(id.toString()));
+            return new ResponseEntity<Map<String,Object>>(response, HttpStatus.NOT_FOUND);
+        }
+
+        if(result.hasErrors()){
+            List<String> errors = new ArrayList<>();
+
+            for(FieldError err: result.getFieldErrors()){
+                errors.add("En el campo: " + err.getField() + " - " +err.getDefaultMessage());
+            }
+            response.put("errors", errors);
+            response.put("msg", "Error al validar la categoria");
+            return new ResponseEntity<Map<String,Object>>(response, HttpStatus.BAD_REQUEST);
+
+        }
+
+
+        categoriaActual.setNombre(categoria.getNombre());
+
+        try {
+            categoriaActual = categoriaService.save(categoriaActual);
+        }catch(DataAccessException e) {
+            response.put("msg","Error al intentar editar la categoria");
+            response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<Map<String,Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        response.put("msg", "Category actualizada correctamente");
+        response.put("categoria", categoriaActual);
+        return new ResponseEntity<Map<String,Object>>(response, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable(value="id") Long id) {
+
+        Map<String,Object> response = new HashMap<>();
+
+        if(categoriaService.findById(id) != null ){
+            try{
+                categoriaService.deleteById(id);
+
+            }catch(DataAccessException e){
+                response.put("msg","Hubo un error al intentar eliminar la categoria");
+                response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+                return new ResponseEntity<Map<String,Object>>(response,HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+
+            response.put("msg","Categoria eliminada correctamente");
+            return new ResponseEntity<Map<String,Object>>(response,HttpStatus.OK);
+
+        }else{
+            response.put("msg","Error tal intentar eliminar la categoria");
+            response.put("error", "No existe una categoria con id = " + id);
+            return new ResponseEntity<Map<String,Object>>(response, HttpStatus.BAD_REQUEST);
+        }
+
     }
 
 
--- src/main/java/uteclab/despensaRincon/controllers/ProductoController.java
+++ src/main/java/uteclab/despensaRincon/controllers/ProductoController.java
@@ -1,13 +1,18 @@
 package uteclab.despensaRincon.controllers;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 import uteclab.despensaRincon.entities.Producto;
 import uteclab.despensaRincon.models.services.ProductoService;
 
+import javax.naming.Binding;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -53,8 +58,54 @@ public class ProductoController {
             return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
         }
         response.put ("msg", "Datos del Producto ingresados correctamente");
-        response.put("Producto", newProducto);
+        response.put("producto", newProducto);
         return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@Valid @RequestBody Producto producto, BindingResult result, @PathVariable(value="id")Long id ) {
+
+        Producto productoActual = productoService.findById(id);
+
+        Map<String,Object> response = new HashMap<>();
+
+        if(productoActual == null) {
+            response.put("msg","No existe un producto con id = ".concat(id.toString()));
+            return new ResponseEntity<Map<String,Object>>(response, HttpStatus.NOT_FOUND);
+        }
+
+        if(result.hasErrors()){
+            List<String> errors = new ArrayList<>();
+
+            for(FieldError err: result.getFieldErrors()){
+                errors.add("In the field: " + err.getField() + " - " +err.getDefaultMessage());
+            }
+            response.put("errors", errors);
+            response.put("msg", "Error al validar el producto");
+            return new ResponseEntity<Map<String,Object>>(response, HttpStatus.BAD_REQUEST);
+
+        }
+
+        productoActual.setNombre(producto.getNombre());
+        productoActual.setCategoria(producto.getCategoria());
+        productoActual.setImagen(producto.getImagen());
+        productoActual.setDescripcion(producto.getDescripcion());
+        productoActual.setPrecio_compra(producto.getPrecio_compra());
+        productoActual.setPrecio_venta(producto.getPrecio_venta());
+        productoActual.setStock_minimo(producto.getStock_minimo());
+        productoActual.setVisible(producto.isVisible());
+
+
+        try {
+            productoActual = productoService.save(productoActual);
+        }catch(DataAccessException e) {
+            response.put("msg","Error al intentar modificar el producto");
+            response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<Map<String,Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        response.put("msg", "Producto actualizado correctamente");
+        response.put("producto", productoActual);
+        return new ResponseEntity<Map<String,Object>>(response, HttpStatus.CREATED);
     }
 
 
