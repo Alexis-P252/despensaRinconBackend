a6ff86a56e72d7cd51d49307c35b64c99f466986
12acastro <88839179+12acastro@users.noreply.github.com>
2023-05-04 21:28:19 -0300

    gitflow-feature-stash: CRUD

---
+++ src/main/java/uteclab/despensaRincon/models/services/IProveedorService.java
@@ -0,0 +1,15 @@
+package uteclab.despensaRincon.models.services;
+
+import uteclab.despensaRincon.entities.Proveedor;
+import uteclab.despensaRincon.entities.Vendedor;
+
+import java.util.List;
+
+public interface IProveedorService {
+    public List<Proveedor> findAll();
+
+    public Proveedor findById(Long id);
+    public Proveedor save (Proveedor proveedor);
+    public void deleteById (Long id);
+    //public void putVendedor (Vendedor vendedor);
+}
