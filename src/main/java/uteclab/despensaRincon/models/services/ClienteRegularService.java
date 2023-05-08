5b40d8c382c26e0d2b870910c543b1b02916ba79
nahup <nahuperdomo123@gmail.com>
2023-05-08 12:18:27 -0300

    Agregando Servicios y controladores de Cliente Regular y de Registro Deuda

---
+++ src/main/java/uteclab/despensaRincon/models/services/ClienteRegularService.java
@@ -0,0 +1,27 @@
+package uteclab.despensaRincon.models.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import uteclab.despensaRincon.entities.ClienteRegular;
+import uteclab.despensaRincon.models.dao.IClienteRegularDao;
+
+import java.util.List;
+
+@Service
+public class ClienteRegularService implements IClienteRegularService{
+    @Autowired
+    private IClienteRegularDao clienteRegularDao;
+    @Override
+    public List<ClienteRegular> findAll() {return (List<ClienteRegular>) clienteRegularDao.findAll();}
+    @Override
+    public ClienteRegular findById(Long id) { return clienteRegularDao.findById(id).orElse(null); }
+
+    @Override
+    public ClienteRegular update(ClienteRegular cr){
+        return clienteRegularDao.update(cr);
+    }
+    @Override
+    public ClienteRegular save(ClienteRegular cr) { return clienteRegularDao.save(cr); }
+    @Override
+    public void deleteById(Long id) { clienteRegularDao.deleteById(id); }
+}
---
+++ src/main/java/uteclab/despensaRincon/models/services/IRegistroDeudaService.java
@@ -0,0 +1,15 @@
+package uteclab.despensaRincon.models.services;
+
+import uteclab.despensaRincon.entities.RegistroDeuda;
+
+import java.util.List;
+
+public interface IRegistroDeudaService  {
+    public List<RegistroDeuda> findAll();
+    public RegistroDeuda findById(Long id);
+    public RegistroDeuda save (RegistroDeuda rd);
+    public void deleteById (Long id);
+
+    //todo
+    // update     public RegistroDeuda update (Long id);
+}
