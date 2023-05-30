552ddb18ddde6f71cac5d699ec680c0779b0fe76
Alexis <aleperalta252@gmail.com>
2023-05-30 17:39:25 -0300

    CRUD de Pedidos

--- src/main/java/uteclab/despensaRincon/models/dao/IClienteRegularDao.java
+++ src/main/java/uteclab/despensaRincon/models/dao/IClienteRegularDao.java
@@ -2,5 +2,11 @@ package uteclab.despensaRincon.models.dao;
 
 import org.springframework.data.repository.CrudRepository;
 import uteclab.despensaRincon.entities.ClienteRegular;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface IClienteRegularDao extends CrudRepository<ClienteRegular, Long> {
+
+    Optional<ClienteRegular> findByCedula(String cedula);
 }
--- src/main/java/uteclab/despensaRincon/models/services/IClienteRegularService.java
+++ src/main/java/uteclab/despensaRincon/models/services/IClienteRegularService.java
@@ -7,6 +7,8 @@ import java.util.List;
 public interface IClienteRegularService {
     public List<ClienteRegular> findAll();
     public ClienteRegular findById(Long id);
+
+    public ClienteRegular findByCedula(String cedula);
     public ClienteRegular save(ClienteRegular cr);
     public void deleteById(Long id);
 
